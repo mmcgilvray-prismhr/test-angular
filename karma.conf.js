63717157f9ca989d14ad3b319856c1c1423f15f7
Michael McGilvray <mmcgilvray@prismhr.com>
2023-09-07 10:12:40 -0400

    update config

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text' },
+        { type: 'lcovonly' },
         { type: 'json-summary' }
       ]
     },
